2056e254f4b6369aee1814a437ea454025e452e7
Airton <airtonmartinsneris@hotmail.com>
2017-11-25 19:39:14 -0300

    Adicionado social login ao app

--- IonicShoppingList/src/app/firebase.credentials.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CREDENTIALS = {
-  apiKey: "AIzaSyBceJixN5xwaQZ-ZDo5orG3DqpE5fGVTgk",
-  authDomain: "promovox-rural.firebaseapp.com",
-  databaseURL: "https://promovox-rural.firebaseio.com",
-  projectId: "promovox-rural",
-  storageBucket: "promovox-rural.appspot.com",
-  messagingSenderId: "355800604533"
-}
