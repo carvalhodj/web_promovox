28c41086d350eea08e43302c4ee59daa4e694af3
Airton <airtonmartinsneris@hotmail.com>
2017-12-05 22:08:16 -0300

    Revert "Adicionado social login ao app"
    This reverts commit 2056e254f4b6369aee1814a437ea454025e452e7.

---
+++ IonicShoppingList/src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+  apiKey: "AIzaSyBceJixN5xwaQZ-ZDo5orG3DqpE5fGVTgk",
+  authDomain: "promovox-rural.firebaseapp.com",
+  databaseURL: "https://promovox-rural.firebaseio.com",
+  projectId: "promovox-rural",
+  storageBucket: "promovox-rural.appspot.com",
+  messagingSenderId: "355800604533"
+}
