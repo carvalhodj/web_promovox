ba0ae1ebfe389ee60e0737b8751302e9dccc7238
Airton <airtonmartinsneris@hotmail.com>
2017-12-10 16:27:41 -0300

    Add search in list

--- IonicShoppingList/src/pages/home/home.ts
+++ IonicShoppingList/src/pages/home/home.ts
@@ -23,5 +23,33 @@ export class HomePage {
                                             ...c.payload.val()
                                           }));
                                         });
+
   }
+
+  getItems(ev) {
+   // Reset items back to all of the items
+   this.shoppingList$ = this.shopping.getShoppingList() // DB List
+                                     .snapshotChanges() // Key and value pairs
+                                     .map(
+                                       changes => {
+                                         return changes.map(c => ({
+                                           key: c.payload.key,
+                                           ...c.payload.val()
+                                         }));
+                                       });
+
+
+   // set val to the value of the ev target
+   var val = ev.target.value;
+
+   // if the value is an empty string don't filter the items
+   if (val && val.trim() != '') {
+     this.shoppingList$ = this.shoppingList$.map( items => items.filter((item) => {
+       return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+     }))
+
+   }
+ }
+
+
 }
